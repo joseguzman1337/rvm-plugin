3228d48d600449a55271f0f598ad406f01d4e95b
jose guzman <joseguzman1337@users.noreply.github.com>
2026-03-31 00:53:06 -0500

    [CRUSH] chore(repo): checkpoint pending changes #TASK-000

--- spec/spec_helper.rb
+++
@@ -1,7 +0,0 @@
-Dir['spec/support/lib/*.jar'].each { |jar| require jar }
-$CLASSPATH << 'spec/support/classes/'
-
-require 'bundler/setup'
-Bundler.require
-
-$: << File.expand_path(File.join(*%w{.. .. models}), __FILE__)
